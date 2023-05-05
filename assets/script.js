d11da50ab14df7e1a6915bdd3403cb616b627a63
antoninodangelo11 <reapercsp@gmail.com>
2023-05-05 15:51:49 +0200

    format code

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        container: 'text-center',
-        startMessage: 'PILLOLA ROSSA',
-        endMessage: 'PILLOLA BLU',
-        colorRed: 'text-danger',
-        space: 'text-dark',
-        colorBlu: 'text-primary',
-        chooseVue: 'HAI SCELTO VUE.JS',
-        chooseJs: 'HAI SCELTO VANILLA.JS',
-        img: 'img/Pills.webp',
-      }
+  data() {
+    return {
+      container: "text-center",
+      startMessage: "PILLOLA ROSSA",
+      endMessage: "PILLOLA BLU",
+      colorRed: "text-danger",
+      space: "text-dark",
+      colorBlu: "text-primary",
+      chooseVue: "HAI SCELTO VUE.JS",
+      chooseJs: "HAI SCELTO VANILLA.JS",
+      img: "img/Pills.webp",
+    };
+  },
+  methods: {
+    pillsRed() {
+      alert(this.chooseVue);
     },
-    methods: {
-        pillsRed() {
-            alert(this.chooseVue);
-        },
-        pillsBlu() {
-            alert(this.chooseJs);
-        },
+    pillsBlu() {
+      alert(this.chooseJs);
     },
+  },
 });
 
 // Spazio lasciato di proposito
 
-app.mount('#app');
+app.mount("#app");
